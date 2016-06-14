a1c9459e0755d4bc5154ae86c254a93566436839
Dimitri Vaughn <dimitrivaughn@Zipcoder27s-MacBook-Pro.local>
2016-06-14 19:11:45 -0400

    Added Singleton classes

--- src/main/java/io/zipcoder/designpatterns/factory/BeerCupDispenser.java
+++ src/main/java/io/zipcoder/designpatterns/factory/BeerCupDispenser.java
@@ -4,5 +4,22 @@ package io.zipcoder.designpatterns.factory;
  * Created by dimitrivaughn on 6/13/16.
  */
 public class BeerCupDispenser extends CupDispenser{
+
+    private static volatile BeerCupDispenser instance;
+    private BeerCupDispenser(){}
+
+    public static BeerCupDispenser getInstance(){
+        if (instance == null) {
+            synchronized (BeerCupDispenser.class){
+                if (instance == null){
+                    instance = new BeerCupDispenser();}
+                }
+            }
+            return instance;
+        }
+
+
+
+
     public BeerCup dispense(){return new BeerCup();}
 }
